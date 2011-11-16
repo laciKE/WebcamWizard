332bde8e78e9306fadc10b6eb7c5f75078ec330c
Ladislav Baco <laciKE3.14@gmail.com>
2011-11-16 23:27:05 +0100

    updated Makefile and main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,21 @@
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
-#include "tabula.hpp"
+#include "blackboard.hpp"
 
 using namespace std;
 
 int main(int argc, char** argv)
 {
 	//vytvorenie a kalibracia novej tabule
-	Tabula * tabula = new Tabula();
-	tabula->calibrate();
+	BlackBoard * blackboard = new BlackBoard();
+	blackboard->initDesktop();
 	//obnovujem obraz na tabuli, kym nie je stlaceny ESC
-	while(tabula->update()) 
+	while(blackboard->update()) 
 	{
 		char c=cvWaitKey(33);
 		if(c==27) break;
 	}
-	delete tabula;
+	delete blackboard;
 
 	return 0;
 }
